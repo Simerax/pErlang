b201b5dc203c2d923455cfd96b1bb4d09ff6377f
Xaver Fischer <Simerax@github.com>
2020-04-29 21:39:45 +0200

    convenience Modul pErlang

---
+++ lib/pErlang.pm
@@ -0,0 +1,81 @@
+package pErlang;
+
+use pErlang::Type qw(:constants);
+use pErlang::Atom;
+use pErlang::Float;
+use pErlang::Integer;
+use pErlang::Binary;
+use pErlang::String;
+use pErlang::Map;
+use pErlang::List;
+use pErlang::Tuple;
+
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(
+    AtomUTF8
+    Atom
+    Float
+    Integer
+    Integer8bit
+    Binary
+    Map
+    List
+    Tuple
+    String
+);
+
+our %EXPORT_TAGS = (
+    all => [@EXPORT_OK]
+);
+
+
+sub AtomUTF8 {
+    my ($name) = @_;
+    return pErlang::Atom->new(name => $name, subtype => ATOM_UTF8_EXT);
+}
+
+sub Atom {
+    my ($name) = @_;
+    return pErlang::Atom->new(name => $name, subtype => ATOM_EXT);
+}
+
+sub Float {
+    my ($value) = @_;
+    return pErlang::Float->new(value => $value, subtype => NEW_FLOAT_EXT);
+}
+
+sub Integer8bit {
+    my ($num) = @_;
+    return pErlang::Integer->new(value => $num, subtype => SMALL_INTEGER_EXT);
+}
+
+sub Integer {
+    my ($num) = @_;
+    print "----------> $num\n";
+    return pErlang::Integer->new(value => $num, subtype => INTEGER_EXT);
+}
+
+sub Binary {
+    my ($data) = @_;
+    return pErlang::Binary->new(data => $data);
+}
+
+sub String {
+    my ($data) = @_;
+    return pErlang::String->new(data => $data);
+}
+
+sub Map {
+    return pErlang::Map->new();
+}
+
+sub List {
+    return pErlang::List->new();
+}
+
+sub Tuple {
+    return pErlang::Tuple->new();
+}
+
+1;
---
+++ t/pErlang/pErlang.t
@@ -0,0 +1,16 @@
+use Test::More tests => 10;
+
+use pErlang qw(:all);
+
+isa_ok(Atom("hi"), "pErlang::Atom");
+isa_ok(AtomUTF8("hello"), "pErlang::Atom");
+isa_ok(Float(2.3), "pErlang::Float");
+isa_ok(Binary("hi there"), "pErlang::Binary");
+isa_ok(String("this is a string"), "pErlang::String");
+isa_ok(Map(), "pErlang::Map");
+isa_ok(List(), "pErlang::List");
+isa_ok(Tuple(), "pErlang::Tuple");
+isa_ok(pErlang::Integer(3), "pErlang::Integer");
+isa_ok(pErlang::Integer8bit(2), "pErlang::Integer");
+
+
