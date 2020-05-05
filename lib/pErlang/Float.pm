c0b21924d1ff4ce9febca5d31be315e155c1dc9a
Xaver Fischer <Simerax@github.com>
2020-05-05 18:58:13 +0200

    float addition

--- lib/pErlang/Float.pm
+++ lib/pErlang/Float.pm
@@ -1,8 +1,13 @@
 package pErlang::Float;
 use Mouse;
 use pErlang::Type qw(:constants);
+use Scalar::Util qw(looks_like_number);
 
 extends 'pErlang::Datastructure';
+
+use overload
+    '+' => \&add,
+    '==' => \&equals,
 
 has value => (
     is => 'rw',
@@ -23,5 +28,30 @@ has subtype => (
         }
     },
 );
+
+sub add {
+    my ($self, $other, $swap) = @_;
+
+    if($swap) {
+        my $tmp = $self;
+        $self = $other;
+        $other = $tmp;
+    }
+
+    return pErlang::Float->new(value => $self->value() + $other, subtype => $self->subtype());
+}
+
+sub equals {
+    my ($self, $other) = @_;
+
+    if(UNIVERSAL::isa($other, 'pErlang::Float') && $other->value() == $self->value()) {
+        return 1;
+    } elsif(looks_like_number($other) && $self->value() == $other) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 
 1;
--- t/pErlang/Float.t
+++ t/pErlang/Float.t
@@ -1,2 +1,11 @@
-use Test::More tests => 1;
+use Test::More tests => 2;
 require_ok('pErlang::Float');
+
+
+
+{
+    my $TEST_NAME = "Float Addition";
+
+    my $float = pErlang::Float->new(value => 3.4);
+    ok($float + 2 == 5.4, $TEST_NAME);
+}
